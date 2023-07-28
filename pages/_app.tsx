4e7a18c5561eb1f3f5106ff5ec76b5a755c79353
Dalufishe <fishawe87@gmail.com>
2023-07-28 14:05:43 +0800

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="dark">
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </div>
   );
