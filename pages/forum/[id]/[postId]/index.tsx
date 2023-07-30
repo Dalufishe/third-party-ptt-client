e212c53be3e9c1e813d8694b0d19255cb4b88248
Dalufishe <fishawe87@gmail.com>
2023-07-31 05:19:42 +0800

    update: image optimization

--- pages/forum/[id]/[postId]/index.tsx
+++ pages/forum/[id]/[postId]/index.tsx
@@ -17,6 +17,25 @@ import Head from "next/head";
 import Navbar from "../../../../components/pages/post-page/Navbar";
 import Image from "next/image";
 
+const convertImage = (w: number, h: number) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`;
+
+const toBase64 = (str: any) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
+
 type Props = {
   post: Post;
 };
@@ -78,10 +97,15 @@ const Page: NextPageWithLayout<Props> = (props: Props) => {
                 <>
                   {img}
                   <Image
+                    priority
                     src={props.post.images[index]}
                     alt={props.post.title + "的照片"}
                     width={500}
                     height={500}
+                    placeholder="blur"
+                    blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                      convertImage(700, 475)
+                    )}`}
                   />
                 </>
               );
