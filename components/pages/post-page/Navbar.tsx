1c84e990840557f85087d23283c8b1c2d72a63e3
Dalufishe <fishawe87@gmail.com>
2023-08-12 17:40:30 +0800

    update: rollback

--- components/pages/post-page/Navbar.tsx
+++ components/pages/post-page/Navbar.tsx
@@ -3,12 +3,6 @@ import { cn } from "../../@/lib/utils";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { FiMoreVertical } from "react-icons/fi";
 import { useRouter } from "next/router";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "../../@/components/ui/dropdown-menu";
 
 type Props = {
   children: ReactNode;
@@ -40,18 +34,9 @@ const Navbar = (props: Props) => {
           {props.children}
         </h2>
       </div>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <div>
-            <FiMoreVertical className={cn("w-5 h-5")} />
-          </div>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="dark:bg-secondary rounded-t-sm">
-          <DropdownMenuItem className="text-base">原版網址</DropdownMenuItem>
-          <DropdownMenuItem className="text-base">複製連結</DropdownMenuItem>
-          <DropdownMenuItem className="text-base">分享</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div>
+        <FiMoreVertical className={cn("w-5 h-5")} />
+      </div>
     </div>
   );
 };
